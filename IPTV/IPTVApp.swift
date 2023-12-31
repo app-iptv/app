bf118412497144ea970d902fd26ce561c08ccbf8
pedrodsac <pedrodsac@gmail.com>
2023-12-31 14:54:08 +0100

    Initial Commit

---
+++ IPTV/IPTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTVApp.swift
+//  IPTV
+//
+//  Created by Pedro Cordeiro on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IPTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
