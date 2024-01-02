00e7e7e3c455da89e8da73c7a4070cef10c5992b
pedrodsac <pedrodsac@gmail.com>
2024-01-02 18:54:18 +0100

    Switched to AZVideoPlayer Switched to M3UKit

--- View/SettingsView.swift
+++ View/SettingsView.swift
@@ -2,40 +2,14 @@
 //  SettingsView.swift
 //  IPTV
 //
-//  Created by Pedro Cordeiro on 31/12/2023.
+//  Created by Pedro Cordeiro on 01/01/2024.
 //
 
 import SwiftUI
-import M3UKit
 
 struct SettingsView: View {
-    
-    @State var enteredURL = ""
-    @StateObject private var appSettings = AppSettings()
-    let viewModel = TVViewModel(appSettings: AppSettings())
-
     var body: some View {
-        NavigationStack {
-            Form {
-                Section(header: Text("Playlist URL")) {
-                    #if DEBUG
-                    HStack {
-                        TextField("Enter Playlist URL", text: $enteredURL)
-                            .disableAutocorrection(true)
-                        Spacer()
-                        Button("Reload Playlist") {
-                            appSettings.IPTVLink = enteredURL
-                            viewModel.loadMediaList()
-                        }
-                    }
-                    Button("Print Playlist") {
-                        print(appSettings.IPTVLink)
-                    }
-                    #endif
-                }
-            }
-            .navigationBarTitle("Settings")
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
--- View/TVView.swift
+++ View/TVView.swift
@@ -2,93 +2,16 @@
 //  TVView.swift
 //  IPTV
 //
-//  Created by Pedro Cordeiro on 31/12/2023.
+//  Created by Pedro Cordeiro on 01/01/2024.
 //
 
 import SwiftUI
-import AVKit
 
 struct TVView: View {
-    
-    @State var showFullscreen = false
-    
-    @ObservedObject private var viewModel = TVViewModel(appSettings: AppSettings())
-    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(viewModel.mediaList) { media in
-                    NavigationLink {
-                        VideoPlayerView(urlString: media.urlString ?? "")
-                    } label: {
-                        Text(media.title ?? "Unknown Title")
-                            .font(.headline)
-                    }
-                }
-                NavigationLink {
-                    VideoPlayer(player: AVPlayer(url: URL(string: "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8")!))
-                } label: {
-                    Text("Test")
-                }
-
-            }
-            .navigationBarTitle("TV Channels")
-        }
-        .onAppear {
-            viewModel.loadMediaList()
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-struct VideoPlayerView: View {
-    
-    @State var showFullscreen = false
-    
-    var urlString: String
-
-    var body: some View {
-        if let url = URL(string: urlString) {
-            VideoPlayer(player: AVPlayer(url: url)) {
-                Text("Video playback failed.")
-            }
-            .navigationBarTitle("Video Player", displayMode: .inline)
-        } else {
-            Text("Invalid URL")
-        }
-    }
-}
-
-class TVViewModel: ObservableObject {
-    @Published var mediaList: [MediaItem] = []
-    @Published var appSettings: AppSettings
-    
-    init(appSettings: AppSettings) {
-        self.appSettings = appSettings
-        // Load media list based on appSettings.m3uFileLink
-    }
-    
-    func loadMediaList() {
-        guard let url = URL(string: appSettings.IPTVLink) else {
-            print("Invalid URL")
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Error fetching data: \(error)")
-                return
-            }
-            
-            if let data = data, let fileContents = String(data: data, encoding: .ascii) {
-                DispatchQueue.main.async {
-                    self.mediaList = MediaItem.parseM3U(contentsOfFile: fileContents)!
-                }
-            }
-        }
-        task.resume()
-    }
-}
-
 
 #Preview {
     TVView()
