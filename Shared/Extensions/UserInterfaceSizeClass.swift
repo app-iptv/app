741df7b756a546e049699739bbf46f0419d0207c
Pedro Cordeiro <pedrodsac@gmail.com>
2024-04-07 16:24:44 +0100

    notuing

---
+++ Shared/Extensions/UserInterfaceSizeClass.swift
@@ -0,0 +1,26 @@
+//
+//  UserInterfaceSizeClass.swift
+//  IPTV App
+//
+//  Created by Pedro Cordeiro on 06/04/2024.
+//
+
+import SwiftUI
+
+extension UserInterfaceSizeClass {
+	var toolbarRole: ToolbarRole {
+		if self == .compact {
+			return ToolbarRole.automatic
+		} else {
+			return ToolbarRole.browser
+		}
+	}
+	
+	var toolbarTitleDisplayMode: ToolbarTitleDisplayMode {
+		if self == .compact {
+			return ToolbarTitleDisplayMode.inline
+		} else {
+			return ToolbarTitleDisplayMode.inlineLarge
+		}
+	}
+}
